69a21774397ca3370ca41a011b207a57ed0387db
ghosh-rouhin <55288458+ghosh-rouhin@users.noreply.github.com>
2023-05-21 20:41:41 +0530

    Update Fincity-Bat programmer in Arkham City needs to help BatMan capture asylum escapees.py

--- Fincity-Bat programmer in Arkham City needs to help BatMan capture asylum escapees.py
+++ Fincity-Bat programmer in Arkham City needs to help BatMan capture asylum escapees.py
@@ -1,5 +1,10 @@
+# Defining the function/method finding the maximum steps
 def Bat_programmer(patient,steps):
+    # Returning the maximum steps required
     return patient * steps
-patient = eval(input())
-steps = eval(input())
+
+patient = eval(input()) # Taking input the number of patient
+steps = eval(input()) # Taking input the number of steps already covered
+
+# Printing the maximu steps
 print(Bat_programmer(patient,steps))
